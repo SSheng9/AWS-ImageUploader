68231a5f9e3f85974a573ca8c63efba27034ff8e
ssa116 <sheng871106@hotmail.com>
2023-01-27 20:21:40 -0800

    aws updated

--- database.js
+++ database.js
@@ -2,24 +2,24 @@ import mysql from "mysql2"
 import dotenv from "dotenv"
 dotenv.config()
 
-// const pool = mysql
-//   .createPool({
-//     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE || 3306,
-//   })
-//   .promise()
-
-  const pool = mysql
+const pool = mysql
   .createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT || 3306,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE || 3306,
   })
   .promise()
+
+  // const pool = mysql
+  // .createPool({
+  //   host: process.env.MYSQLHOST,
+  //   user: process.env.MYSQLUSER,
+  //   password: process.env.MYSQLPASSWORD,
+  //   database: process.env.MYSQLDATABASE,
+  //   port: process.env.MYSQLPORT || 3306,
+  // })
+  // .promise()
 
 
   
